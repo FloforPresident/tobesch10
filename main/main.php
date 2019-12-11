52c37e5ae1aff862ce66869c3e863a676014390c
FloforPresident <florian.geiger@kurzdigital.com>
2019-12-11 21:26:23 +0100

    bissl Startseite

--- main/main.php
+++ main/main.php
@@ -5,5 +5,19 @@
     </head>
     <body>
         <?php include "../nav/nav.php" ?>
+
+        <main>
+            <section class="feed">
+                <h2>Cool Kids Dance like Zyzz</h2>
+                <p>Bei den coolen Kids steht der so genannte Zyzz - Dance hoch im Kurz, hör auf zu twerken und Dance like Zyzz #Sickkunt</p>
+
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/iht8pmM8Eeo" 
+                    frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </section>
+            <section class="feed">
+                <h2>Bullshit of the Week</h2>
+                <p>Vote für den größten Bullshit dieser Woche</p>
+            </section>
+        </main>
     </body>
 </html>
